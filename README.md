f0ec72f0d49893c92c9306c72da6d1110537ac40
Aissa Feldmann <aissafeldmann@Aissas-Air.attlocal.net>
2020-10-26 11:57:05 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # optimizedTravel
+A full stack app to build optimized driving routes for traveling nurses.
+
+TODO
+* [x] Develop user stories
+
+* [x] Set up server
+    * [x] Install dependencies
+    * [x] Install & set up linter
+    * [x] Set up Express appß
+
+* [] Develop database model
+    * [] What data will be stored?
+    * [] Set up mysql models
+    * [] POST request to fakr to generate fake patient location data   
---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+//"api" as the prefix to all routes is set here
+// app.use('/api/', allRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static("client/build"));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
